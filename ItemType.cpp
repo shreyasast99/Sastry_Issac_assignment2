805f9fdc6edf590e27339ea984f26ec1a3a32f8c
Shreya Sastry <srs93414@uga.edu>
2020-02-18 20:37:46 -0500

    added to itemtype.h and .cpp files

--- ItemType.cpp
+++ ItemType.cpp
@@ -1,1 +1,14 @@
 #include "ItemType.h"
+#include  <iostream>
+RelationType Itemtype::ComparedTo(ItemType otherItem) const {
+    if  ( value  <  otherItem.value )
+        return  LESS;
+    else if (value  > otherItem.value )
+        return  GREATER;else  return  EQUAL;}
+void ItemType::Print ( ) const {
+    using namespace std;
+    cout  <<  value  <<  endl;
+}
+void ItemType::Initialize ( int  number ){
+    value  =  number;
+} 
--- ItemType.h
+++ ItemType.h
@@ -1,1 +1,13 @@
 #include <iostream>
+const int  MAX_ITEM = 5 ;
+enum  RelationType { LESS, EQUAL, GREATER };
+class  ItemType// declares class data type
+{
+public : //  3 public member functions
+    RelationType  ComparedTo ( ItemType )  const;
+    void    Print ( )  const;
+    void    Initialize ( int  number ) ;
+private :// 1 private data member
+    int value ;           // could be any different
+// type, including a class
+} ;
